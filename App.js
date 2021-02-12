12e78a537a2aea7d89a8d4f2f79a1e9bb975a61e
sschneeberg <simone.m.schneeberg@gmail.com>
2021-02-12 14:45:57 -0500

    set up app

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}>
+                <Text>Universal React with Expo</Text>
+            </View>
+        </NavigationContainer>
+    );
 }
