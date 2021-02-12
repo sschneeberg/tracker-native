349843a340cc20303927ca18b45ca71f497544d5
sschneeberg <simone.m.schneeberg@gmail.com>
2021-02-12 15:34:11 -0500

    follow set up from tutorial

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function DefaultScreen({ navigation }) {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100px'
+            }}>
+            <Text>Universal React with Expo</Text>
+            <Button title="More" onPress={() => navigation.navigate('NewPage')} />
+        </View>
+    );
+}
+
+function NewScreen({ navigation }) {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+            <Text>New Page with React Navigator</Text>
+            <Button title="Home" onPress={() => navigation.goBack()} />
+        </View>
+    );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
         <NavigationContainer>
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: 'center',
-                    alignItems: 'center'
-                }}>
-                <Text>Universal React with Expo</Text>
-            </View>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="CRNA-Default" component={DefaultScreen} />
+                <Stack.Screen name="NewPage" component={NewScreen} />
+            </Stack.Navigator>
         </NavigationContainer>
     );
 }
